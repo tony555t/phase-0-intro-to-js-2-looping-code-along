106721e443489934c049657c7a88dd823858dbb9
tony555t <tonytullah764@gmail.com>
2022-11-28 07:40:48 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+ 
+function writeCards(names,event){
+    const messages = [];
+    for(let i = 0;  i < 3; i++ ){
+        const greetingMessage = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(greetingMessage);
+    }
+return messages;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+
+//countdown  function
+
+function countDown(){
+let i = 10;
+while(i >= 0){
+    console.log(i);
+    i--;
+}
+}
+
+countDown();
